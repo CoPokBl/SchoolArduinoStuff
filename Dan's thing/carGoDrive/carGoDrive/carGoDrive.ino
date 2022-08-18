feb7614c60f767d86df1257755678a67886c18ca
Ventrix <daniel.shamir125@gmail.com>
2022-08-18 11:45:51 +1000

    Made carGoDrive.ino better

--- Dan's thing/carGoDrive/carGoDrive/carGoDrive.ino
+++ Dan's thing/carGoDrive/carGoDrive/carGoDrive.ino
@@ -1,31 +1,20 @@
-/*
-  SparkFun Inventor’s Kit
-  Circuit 5A - Motor Basics
-  Learn how to control one motor with the motor driver.
-  This sketch was written by SparkFun Electronics, with lots of help from the Arduino community.
-  This code is completely free for any use.
-  View circuit diagram and instructions at: https://learn.sparkfun.com/tutorials/sparkfun-inventors-kit-experiment-guide---v41
-  Download drawings and code at: https://github.com/sparkfun/SIK-Guide-Code
-*/
-
-//PIN VARIABLES
-//the motor will be controlled by the motor A pins on the motor driver
-const int AIN1 = 12;           //control pin 1 on the motor driver for the right motor
-const int AIN2 = 13;    
-const int BIN1 = 9;           //control pin 1 on the motor driver for the right motor
-const int BIN2 = 8;            //control pin 2 on the motor driver for the right motor
+const int AIN1 = 12;
+const int AIN2 = 13;
+const int BIN1 = 9;
+const int BIN2 = 8;
 const int PWMA = 11;  
-const int PWMB = 10;             //speed control pin on the motor driver for the right motor
+const int PWMB = 10;
+int switchPin = 7;
 
-int switchPin = 7;             //switch to turn the robot on and off
+int leftDrv = 0;
+int rightDrv = 0;
 
-//VARIABLES
-int motorSpeed = 0;       //starting speed for the motor
+int swtch = false;
+int x = 0;
 
 void setup() {
-  pinMode(switchPin, INPUT_PULLUP);   //set this as a pullup to sense whether the switch is flipped
-
-  //set the motor control pins as outputs
+  pinMode(switchPin, INPUT_PULLUP);
+  
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   pinMode(BIN1, OUTPUT);
@@ -34,52 +23,62 @@ void setup() {
   pinMode(PWMB, OUTPUT);
 
   Serial.begin(9600);                       //begin serial communication with the computer
-
-  Serial.println("Enter motor speed (0-255)... ");    //Prompt to get input in the serial monitor.
 }
 
 void loop() {
-  motorSpeed = 255;
-  if (Serial.available() > 0) {         //if the user has entered something in the serial monitor
-    motorSpeed = Serial.parseInt();     //set the motor speed equal to the number in the serial message
-
-    Serial.print("Motor Speed: ");      //print the speed that the motor is set to run at
-    Serial.println(motorSpeed);
+  if (swtch == false) {
+    x += 1;
+  }
+  else if (swtch == true) {
+    x -= 1;
   }
 
+  if (x >= 350) {
+    swtch = true;
+  }
+  else if (x <= -350) {
+    swtch = false;
+  }
+
+  leftDrv = x;
+  rightDrv = x;
+
   if (digitalRead(7) == LOW) {          //if the switch is on...
-    spinMotor(motorSpeed);
+    spinMotor(leftDrv, rightDrv);
   } else {                              //if the switch is off...
-    spinMotor(0);                   //turn the motor off
+    spinMotor(0, 0);                   //turn the motor off
   }
 
 
 }
 
-/********************************************************************************/
-void spinMotor(int motorSpeed)                       //function for driving the right motor
-{
-  if (motorSpeed > 0)                                 //if the motor should drive forward (positive speed)
-  {
-    digitalWrite(AIN1, HIGH);                         //set pin 1 to high
+void spinMotor(int leftDrv, int rightDrv) {
+  if (leftDrv > 0) {
+    digitalWrite(AIN1, HIGH);
     digitalWrite(AIN2, LOW);    
-    //digitalWrite(BIN1, HIGH);                         //set pin 1 to high
-    digitalWrite(BIN2, 50);                          //set pin 2 to low
   }
-  else if (motorSpeed < 0)                            //if the motor should drive backward (negative speed)
-  {
-    digitalWrite(AIN1, LOW);                          //set pin 1 to low
+  else if (leftDrv < 0) {
+    digitalWrite(AIN1, LOW);
     digitalWrite(AIN2, HIGH); 
-    digitalWrite(BIN1, LOW);                         //set pin 1 to high
-    digitalWrite(BIN2, LOW);                          //set pin 2 to high
   }
-  else                                                //if the motor should stop
-  {
-    digitalWrite(AIN1, LOW);                          //set pin 1 to low
+  else {
+    digitalWrite(AIN1, LOW);
     digitalWrite(AIN2, LOW);         
-    digitalWrite(AIN1, LOW);                          //set pin 1 to low
-    digitalWrite(AIN2, LOW);                          //set pin 2 to low
   }
-  analogWrite(PWMA, abs(motorSpeed));  
-  analogWrite(PWMB, abs(motorSpeed));     //now that the motor direction is set, drive it at the entered speed
+  
+  if (rightDrv > 0) {
+    digitalWrite(BIN1, HIGH);
+    digitalWrite(BIN2, LOW);
+  }
+  else if (rightDrv < 0) {
+    digitalWrite(BIN1, LOW)
+    digitalWrite(BIN2, HIGH);
+  }
+  else { 
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, LOW);
+  }
+  
+  analogWrite(PWMA, abs(leftDrv));  
+  analogWrite(PWMB, abs(rightDrv));
 }
